86bea71bc355037a005c51da5c25894d643fe27d
ma125120 <1418205177@qq.com>
2019-08-04 21:36:49 +0800

    优化： 提取exec

--- util/index.js
+++ util/index.js
@@ -2,6 +2,9 @@ const path = require("path");
 const fs = require("fs");
 const os = require("os");
 const isWin = os.platform() === `win32`;
+const promisify = require("util").promisify;
+let { exec } = require("child_process");
+exec = promisify(exec);
 
 function getHttpPort() {
   const home = os.homedir();
@@ -33,5 +36,6 @@ function getDist(config = {}) {
 module.exports = {
   getHttpPort,
   getPath,
-  getDist
+  getDist,
+  exec
 };
