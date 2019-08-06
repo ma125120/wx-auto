6d329aa24bf35ccfc06e531a7c5717debc92ab23
ma125120 <1418205177@qq.com>
2019-08-06 17:21:20 +0800

    fix: 初始无法获取port

--- auto.js
+++ auto.js
@@ -1,13 +1,13 @@
 module.exports = {
   cli: "D:/soft/微信web开发者工具", // cli文件所在的目录
-  projectpath: `D:/www/react/heywoof-app-frontend`, // 项目地址
+  projectpath: `D:/www/heywoof-app-frontend`, // 项目地址
   compile: {
-    pathName: `pages/scene/index`, // 自动预览的页面路径
-    query: `activityId=5d45050569515b000c5b740a` // 查询参数，微信目前有BUG，只能识别一个参数
+    pathName: `pages/home/index`, // 自动预览的页面路径
+    query: `` // 查询参数，微信目前有BUG，只能识别一个参数
   },
   build: `yarn build-test:weapp`, // 上传之前需要执行的命令
   upload: {
     version: "1.0.1",
-    desc: "测试自动上传，不要乱动"
+    desc: "热区 上传测试"
   }
 };
--- bin/index.js
+++ bin/index.js
@@ -40,7 +40,7 @@ function start() {
     /\"/g,
     `\\"`
   );
-  config.port = getHttpPort();
+  // config.port = getHttpPort();
 
   run(config, program.type);
 }
